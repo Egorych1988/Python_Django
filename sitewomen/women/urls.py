6c0731d1e76d000086dcefb7bb383ab30267e73c
Egor <Egorych1988@mail.ru>
2024-11-27 00:16:45 +0300

    Add files1

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -6,9 +6,9 @@ from . import views, converters
 register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
-    path('', views.index),
-    path('cats/<int:cat_id>/', views.categories),
-    path('cats/<slug:cat_slug>/', views.categories_by_slug),
-    path('archive/<year4:year>/', views.archive),
-    #re_path(r'^archive/(?P<year>[0-9]{4})/', views.archive),
+    path('', views.index, name='home'),
+    path('cats/<int:cat_id>/', views.categories, name='cats_id'),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats_slug'),
+    path('archive/<year4:year>/', views.archive, name='archive'),
+    # re_path(r'^archive/(?P<year>[0-9]{4})/', views.archive),
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,25 +1,37 @@
-from django.http import HttpResponse, HttpRequest, HttpResponseNotFound, Http404
+from django.http import HttpResponse, HttpRequest, HttpResponseNotFound, Http404, HttpResponseRedirect, \
+    HttpResponsePermanentRedirect
 from django.shortcuts import render, redirect
+from django.urls import reverse
+
 
 def index(request):
     return HttpResponse('Страница приложения women.')
 
+
 def categories(request, cat_id):
     return HttpResponse(f'<h1>Статьи по категориям</h1><p>id: {cat_id}</p>')
+
 
 def categories_by_slug(request, cat_slug):
     if request.GET:
         print(request.GET)
     return HttpResponse(f'<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>')
 
+
 def archive(request, year):
     if year > 2023:
-        return redirect('/')
+        uri = reverse('cats_slug', args=('music',) )
+        return HttpResponseRedirect(uri)
+        # return HttpResponsePermanentRedirect('/') #код 301
+        # return redirect('home') #код 302
+        # return redirect(index)
+        # return redirect(categories_by_slug, '2gfg00', permanent=True) #код 301
+        # return redirect('/')
         # raise Http404()
     return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+
 
 def page_not_found(request, exception):
     return HttpResponseNotFound(f'<h1>Страница не найдена</h1>')
 
 # Create your views here.
-
