b0fcf9594a98e80903beb30831fee6f4a8802fdb
Egor <Egorych1988@mail.ru>
2024-11-27 22:21:30 +0300

    Add files1

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,11 +1,35 @@
 from django.http import HttpResponse, HttpRequest, HttpResponseNotFound, Http404, HttpResponseRedirect, \
     HttpResponsePermanentRedirect
 from django.shortcuts import render, redirect
+from django.template.defaultfilters import title
 from django.urls import reverse
+from django.template.loader import render_to_string
+
+menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+
+
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
 
 
 def index(request):
-    return HttpResponse('Страница приложения women.')
+    data = {'title': 'Главная страница',
+            'menu': menu,
+            'flout': 28.56,
+            'lst': [1, 2, 'abc', True],
+            'set': {1, 2, 3, 2, 5},
+            'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
+            'obj': MyClass(10,20)
+            }
+    return render(request, 'women/index.html', context=data)
+    # t = render_to_string('women/index.html')
+    # return HttpResponse(t)
+
+
+def about(request):
+    return render(request, 'women/about.html', {'title': 'О сайте'})
 
 
 def categories(request, cat_id):
@@ -20,7 +44,7 @@ def categories_by_slug(request, cat_slug):
 
 def archive(request, year):
     if year > 2023:
-        uri = reverse('cats_slug', args=('music',) )
+        uri = reverse('cats_slug', args=('music',))
         return HttpResponseRedirect(uri)
         # return HttpResponsePermanentRedirect('/') #код 301
         # return redirect('home') #код 302
